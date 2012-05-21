4817b4b798eb2fe616575c27e290f747ed4484ed
MIn Cai <min.cai.china@gmail.com>
2012-05-21 13:30:07 +0800

    Rename package net.pickapack.util.* to net.pickapack.*.

---
+++ src/main/java/net/pickapack/RoundRobinScheduler.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2012 by Min Cai (min.cai.china@gmail.com).
+ *
+ * This file is part of the PickaPack library.
+ *
+ * PickaPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PickaPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PickaPack. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package net.pickapack;
+
+import net.pickapack.action.Function1;
+import net.pickapack.action.Predicate;
+
+import java.util.BitSet;
+import java.util.List;
+
+public class RoundRobinScheduler<ResourceT> {
+    private final List<ResourceT> resources;
+    private final Predicate<ResourceT> pred;
+    private final Function1<ResourceT, Boolean> consumeAction;
+    private final int quant;
+
+    private int resourceId;
+
+    private BitSet stalled;
+
+    public RoundRobinScheduler(List<ResourceT> resources, Predicate<ResourceT> pred, Function1<ResourceT, Boolean> consumeAction, int quant) {
+        this.resources = resources;
+        this.pred = pred;
+        this.consumeAction = consumeAction;
+        this.quant = quant;
+
+        this.resourceId = 0;
+
+        this.stalled = new BitSet(this.resources.size());
+    }
+
+    public void consumeNext() {
+        this.resourceId = consumeNext(this.resourceId);
+    }
+
+    private int findNext(BitSet except) {
+        for (int i = 0; i < this.resources.size(); i++) {
+            if (this.pred.apply(this.resources.get(i)) && !except.get(i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private int consumeNext(int resourceId) {
+        this.stalled.clear();
+
+        resourceId = (resourceId + 1) % this.resources.size();
+
+        for (int numConsumed = 0; numConsumed < this.quant; numConsumed++) {
+            if (this.stalled.get(resourceId) || !this.pred.apply(this.resources.get(resourceId))) {
+                resourceId = findNext(this.stalled);
+            }
+
+            if (resourceId == -1) {
+                break;
+            }
+
+            if (!this.consumeAction.apply(this.resources.get(resourceId))) {
+                this.stalled.set(resourceId);
+            }
+        }
+
+        return resourceId;
+    }
+}
---
+++ src/main/java/net/pickapack/im/sink/GracefulMessageSink.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2012 by Min Cai (min.cai.china@gmail.com).
+ *
+ * This file is part of the PickaPack library.
+ *
+ * PickaPack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PickaPack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PickaPack. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package net.pickapack.im.sink;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GracefulMessageSink implements MessageSink {
+    private MessageSink proxy;
+
+    public GracefulMessageSink(MessageSink proxy) {
+        this.proxy = proxy;
+    }
+
+    @Override
+    public Set<String> getUserIds() {
+        try {
+            return this.proxy.getUserIds();
+        } catch (Exception e) {
+            System.err.println(e);
+            return new HashSet<String>();
+        }
+    }
+
+    @Override
+    public void send(String fromUserId, String toUserId, String message) {
+        try {
+            this.proxy.send(fromUserId, toUserId, message);
+        } catch (Exception e) {
+            System.err.println(e); //TODO: schedule resend
+        }
+    }
+
+    @Override
+    public String receive(String userId) {
+        try {
+            return this.proxy.receive(userId);
+        } catch (Exception e) {
+            System.err.println(e);
+            return null;
+        }
+    }
+}
